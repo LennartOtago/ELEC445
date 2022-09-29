45bad8b1324f3daf320c7d4af941a2f1a7e156da
LennartOtago <lennart@postgrad.otago.ac.nz>
2022-09-29 17:36:38 +1300

    before Friday

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import numpy as np
 from PIL import Image, ImageOps
 from numpy import array
 import cv2
+from scipy import signal
 
 import matplotlib.pyplot as plt
 
@@ -66,86 +67,77 @@ for (i, M) in zip([0,1,2], [10, 30, 100]):
     #print(np.sum(s>1e-6))
 
 
+
+################################################################
+
 im = Image.open('jupiter1.tif')
 #im.show()
 
-#print(im.size)
-
 gray_img = ImageOps.grayscale(im)
-#gray_img.show()
-A = array(gray_img)
+gray_img.show()
 
-#Z = np.zeros((256,256))
-
-#blurred image
-A_blurred = A *1/25
-img = Image.fromarray(A_blurred)
-img.show()
+#get psf from satellite
+org_img = array(gray_img)
 xpos = 234
 ypos = 85 # Pixel at centre of satellite
-
-print((xpos-16, 256-(xpos + 16) ))
-print(ypos-16, 256-(ypos + 16) )
+sat_img = org_img[ypos-16: ypos + 16, xpos-16 :xpos+16]
 
 
 
+#blurred image
+A_blurred = org_img * 1 / 25
+img = Image.fromarray(A_blurred)
 
-sat_img = A[ypos-16: ypos + 16, xpos-16 :xpos+16]
-
-#sat_img = sat_img/np.amax(sat_img)
-sat_img = sat_img/sum(sum(sat_img))
-
-#padded_A = np.pad(A[ypos-16: ypos + 16, xpos-16 :xpos+16], ((69,155), (218,6)) )
-
-padded_img = np.pad(sat_img, ((69,155), (218,6)) )
-
-#img = Image.fromarray(padded_img)
-#img.show()
-
-
-
-fourier_img = np.fft.fft2(padded_img)
-print(fourier_img[0,20])
-
-magn_psf = np.abs(fourier_img)
-
-
-print(np.amax(np.abs(fourier_img)))
-x= np.linspace(0,255,256)
-y= np.linspace(0,255,256)
-
-X,Y = np.meshgrid(x,y)
-
-#fig = plt.figure()
-#ax = fig.add_subplot(projection = '3d')
-#ax.plot_surface(X,Y, magn_psf)
-#fig.show()
-
-
-
-decov_img = np.fft.ifft2(np.fft.fft2(A)/fourier_img)
-img = Image.fromarray(np.abs(decov_img))
-#img.show()
-
-
+#convolved image with modified with normalized psf
+sat_img = sat_img/(sum(sum(sat_img)))
+A_conv = signal.convolve2d(org_img, sat_img, mode='same')
+img = Image.fromarray(A_conv)
 #img = Image.fromarray(sat_img)
 #img.show()
 
+#zero pad image so padded_img has same size as org_img
+padded_img = np.pad(sat_img, ((69,155), (218,6)) )
+img = Image.fromarray(padded_img)
+#img.show()
 
+
+#fourier transfrom padded image
+fourier_img = np.fft.fft2(padded_img)
+img = Image.fromarray(np.abs(fourier_img))
+#img.show()
+
+#get singular values
 #magnitude of finite fourier transform in descending order
-
+magn_psf = np.abs(fourier_img)
 sorted_psf = np.sort(magn_psf, axis=None)
 fig, ax = plt.subplots()
 ax.plot(range(0,len(sorted_psf)), sorted_psf[::-1])
 ax.set_yscale('log')
 #plt.show()
 
-print(sorted_psf)
+#plot magnitude
+x= np.linspace(0,255,256)
+y= np.linspace(0,255,256)
+X,Y = np.meshgrid(x,y)
+fig = plt.figure()
+ax = fig.add_subplot(projection = '3d')
+ax.plot_surface(X,Y, magn_psf)
+#fig.show()
 
-test = np.abs(fourier_img * np.conj(fourier_img))
 
-lam = 0.8
-tikh_img = np.fft.ifft2(np.fft.fft2(A) * np.conj(fourier_img)/(lam**2 + test ) )
-img = Image.fromarray(np.abs(tikh_img))
+#naive inversion by direct division
+decov_img = np.fft.ifft2(np.fft.fft2(A_conv)/fourier_img)
+img = Image.fromarray(np.abs(decov_img))
 img.show()
+
+
+
+
+norm_psf = fourier_img * np.conj(fourier_img)
+
+lam = 0.5
+tikh_img = np.fft.ifft2(np.fft.fft2(A_conv) * np.conj(fourier_img).transpose() /(lam**2 + norm_psf.transpose() ) )
+img = Image.fromarray(np.abs(tikh_img))
+#img.show()
 #h = h./sum(sum(h));
+
